9ea1e01854a22216a88a37a2ea024503d9133dff
purdurnr <kyle.dando@nxp.com>
2023-01-24 17:10:25 -0600

    Add LPC55S69

---
+++ examples/issdk/sensors/fxls8962/interrupt/RTE/_fxls8962_interrupt.release_frdmk22f_agmp03/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'fxls8962_interrupt.release+frdmk22f_agmp03' 
+ * Target:  'fxls8962_interrupt.release+frdmk22f_agmp03' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "fsl_device_registers.h"
+
+/* NXP::Device:SDK Utilities:serial_manager_uart:1.0.0 */
+#ifndef SERIAL_PORT_TYPE_UART
+#define SERIAL_PORT_TYPE_UART 1
+#endif
+
+
+#endif /* RTE_COMPONENTS_H */
