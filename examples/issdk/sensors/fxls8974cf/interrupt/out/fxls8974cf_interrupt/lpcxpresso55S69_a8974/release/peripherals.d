4764cba291cf729c3e405d55bad37845d55221f2
Robert Rostohar <robert.rostohar@arm.com>
2023-01-27 13:09:03 +0100

    Remove generated files

--- examples/issdk/sensors/fxls8974cf/interrupt/out/fxls8974cf_interrupt/lpcxpresso55S69_a8974/release/peripherals.d
+++
@@ -1,3 +0,0 @@
-out/fxls8974cf_interrupt/lpcxpresso55s69_a8974/release/peripherals.o: \
-  RTE\Board_Support\LPC55S69JBD100_cm33_core0\peripherals.c \
-  RTE\Board_Support\LPC55S69JBD100_cm33_core0\peripherals.h
