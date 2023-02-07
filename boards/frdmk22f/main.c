1dfb57a8d9201e4c7661627973a0f6a67b638b74
Robert Rostohar <robert.rostohar@arm.com>
2023-02-07 13:02:28 +0100

    Update frdmk22f Board layer
    - 'main' function is part of Board layer
      - Initialize the hardware
      - run 'app_main' (application)
    - retarget STDIO

---
+++ boards/frdmk22f/main.c
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------
+ * Copyright (c) 2023 Arm Limited (or its affiliates). 
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *---------------------------------------------------------------------------*/
+
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "systick_utils.h"
+#include "main.h"
+
+int main (void) {
+
+  /*! Initialize the MCU hardware. */
+  BOARD_InitPins();
+  BOARD_BootClockRUN();
+  BOARD_SystickEnable();
+  BOARD_InitDebugConsole();
+
+  SystemCoreClockUpdate();
+
+  return (app_main());
+}
