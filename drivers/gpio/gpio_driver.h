1dd31242cf0a7ac61a603fdfb7b8b57f1ba2c013
Robert Rostohar <robert.rostohar@arm.com>
2023-02-14 10:16:05 +0100

    Add generic GPIO driver (proposal)

---
+++ drivers/gpio/gpio_driver.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * @file gpio_driver.h
+ * @brief The gpio_driver.h file contains the GPIO API.
+*/
+
+#ifndef __DRIVER_GPIO_H__
+#define __DRIVER_GPIO_H__
+
+#include "Driver_GPIO.h"
+
+extern ARM_DRIVER_GPIO Driver_GPIO;
+
+#endif // __DRIVER_GPIO_H__
