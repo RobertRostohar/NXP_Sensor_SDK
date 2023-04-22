a10e3cb5c33d461689bf2722ebddf81c52ea758e
purdurnr <kyle.dando@nxp.com>
2023-04-21 22:32:54 -0500

    Added Shield: FRDMSTBC-P3115 (MPL3115A)

---
+++ shields/p3115/frdm_stbc_p3115_shield.c
@@ -0,0 +1,40 @@
+/*---------------------------------------------------------------------------
+ * Copyright (c) 2023 Arm Limited (or its affiliates).
+ * All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *---------------------------------------------------------------------------*/
+
+#include CMSIS_board_header
+#include "frdm_stbc_p3115_shield.h"
+#include "Driver_GPIO.h"
+
+extern ARM_DRIVER_GPIO Driver_GPIO0;
+
+// Shield Setup (default configuration)
+int32_t shield_setup (void) {
+  ARM_DRIVER_GPIO *pGpio = &Driver_GPIO0;
+
+  // ARDUINO_UNO_D2  - INT2_3115 (MPL3115A): Input, No Pull Resistor
+  #ifdef ARDUINO_UNO_D2
+  pGpio->Setup(ARDUINO_UNO_D2,  NULL);
+  #endif
+
+  // ARDUINO_UNO_D5  - INT1_3115 (MPL3115A): Input, No Pull Resistor
+  #ifdef ARDUINO_UNO_D5
+  pGpio->Setup(ARDUINO_UNO_D5,  NULL);
+  #endif
+
+  return 0;
+}
