c307fe7cccadc1ac23ce9b92aec57a18b5de5dce
Robert Rostohar <robert.rostohar@arm.com>
2023-05-29 11:47:46 +0200

    Update A8974 Shield (board specific handling)

--- shields/a8974/frdm_stbi_a8974_shield.c
+++ shields/a8974/frdm_stbi_a8974_shield.c
@@ -44,16 +44,31 @@ int32_t shield_setup (void) {
   // ARDUINO_UNO_D5  - INTF_SEL (FXLS8974): Input, No Pull Resistor
   #ifdef ARDUINO_UNO_D5
   pGpio->Setup(ARDUINO_UNO_D5,  NULL);
-  #ifdef IMX_1060
+  #if defined(FXLS8974_I2C)
+  #if defined(IMX_1060) || defined(LPC_55S69)
   // Manually drive pin low (shield pulldown is weaker than board pull-up)
   pGpio->SetDirection(ARDUINO_UNO_D5, ARM_GPIO_OUTPUT);
   pGpio->SetOutput(ARDUINO_UNO_D5, 0U);
+  #endif
   #endif
   #endif
 
   // ARDUINO_UNO_D6  - BT_MODE (FXLS8974): Input, No Pull Resistor
   #ifdef ARDUINO_UNO_D6
   pGpio->Setup(ARDUINO_UNO_D6,  NULL);
+  #endif
+
+  // ARDUINO_UNO_D12 - SA0_MISO (FXLS8974): Input, No Pull Resistor
+  #ifdef ARDUINO_UNO_D12
+  pGpio->Setup(ARDUINO_UNO_D12, NULL);
+  #if defined(FXLS8974_I2C)
+  #if defined(LPC_55S69)
+  // Manually drive pin low (shield pulldown is weaker than board-pull up)
+  pGpio->Setup(ARDUINO_UNO_D12, NULL);
+  pGpio->SetDirection(ARDUINO_UNO_D12, ARM_GPIO_OUTPUT);
+  pGpio->SetOutput(ARDUINO_UNO_D12, 0U);
+  #endif
+  #endif
   #endif
 
   // ARDUINO_UNO_D17 - WAKEUP (FXLS8974): Input, No Pull Resistor
